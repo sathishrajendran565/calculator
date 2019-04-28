1bda2fad5c18e83d8cc5223317b60802129c4948
sathish <sathish.rajendran@rebtel.com>
2019-04-28 14:48:50 +0200

    Adding files for a simple calculator

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,102 @@
-import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async, ComponentFixture } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { AppComponent } from "./app.component";
+import { FormsModule } from "@angular/forms";
+import { DebugElement, asNativeElements } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [AppComponent]
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it("## should create the app", () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'calculator'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('calculator');
+  it("## should create the app and initialize controls with default values", () => {
+    fixture.componentInstance.ngOnInit();
+    fixture.componentInstance.selected = 1;
+    fixture.componentInstance.result = 0;
+    fixture.detectChanges();
+    var resultlabel = fixture.debugElement.query(By.css("label.result"));
+    expect(resultlabel.nativeElement.textContent).toBe("Result: 0");
+    fixture.whenStable().then(() => {
+      var inputRadio = fixture.debugElement.query(By.css("input.sum-radio"));
+      expect(inputRadio.nativeElement.checked).toBe(true);
+    });
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it("## should add three numbers correctly", () => {
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to calculator!');
+    const firstInput = fixture.debugElement.query(By.css("#firstNumber"))
+      .nativeElement;
+    const secondInput = fixture.debugElement.query(By.css("#secondNumber"))
+      .nativeElement;
+    const thirdInput = fixture.debugElement.query(By.css("#thirdNumber"))
+      .nativeElement;
+    var resultlabel = fixture.debugElement.query(By.css("label.result"));
+
+    firstInput.value = 1;
+    secondInput.value = 2;
+    thirdInput.value = 3;
+    fixture.componentInstance.selected = 1;
+
+    firstInput.dispatchEvent(new Event("input"));
+    secondInput.dispatchEvent(new Event("input"));
+    thirdInput.dispatchEvent(new Event("input"));
+
+    fixture.componentInstance.updateResult(
+      firstInput.value,
+      secondInput.value,
+      thirdInput.value
+    );
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(firstInput.value).toEqual("1");
+      expect(secondInput.value).toEqual("2");
+      expect(thirdInput.value).toEqual("3");
+      expect(resultlabel.nativeElement.textContent).toBe("Result: 6");
+    });
+  });
+
+  it("## should multiply three numbers correctly", () => {
+    fixture.detectChanges();
+    const firstInput = fixture.debugElement.query(By.css("#firstNumber"))
+      .nativeElement;
+    const secondInput = fixture.debugElement.query(By.css("#secondNumber"))
+      .nativeElement;
+    const thirdInput = fixture.debugElement.query(By.css("#thirdNumber"))
+      .nativeElement;
+    var resultlabel = fixture.debugElement.query(By.css("label.result"));
+
+    firstInput.value = 1;
+    secondInput.value = 4;
+    thirdInput.value = 3;
+    fixture.componentInstance.selected = 2;
+
+    firstInput.dispatchEvent(new Event("input"));
+    secondInput.dispatchEvent(new Event("input"));
+    thirdInput.dispatchEvent(new Event("input"));
+
+    fixture.componentInstance.updateResult(
+      firstInput.value,
+      secondInput.value,
+      thirdInput.value
+    );
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(firstInput.value).toEqual("1");
+      expect(secondInput.value).toEqual("4");
+      expect(thirdInput.value).toEqual("3");
+      expect(resultlabel.nativeElement.textContent).toBe("Result: 12");
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {
-  title = 'calculator';
+export class AppComponent implements OnInit {
+  title = "calculator";
+  selected;
+
+  ngOnInit() {
+    this.selected = 1;
+    this.result = 0;
+  }
+
+  result: number;
+  Add(first: number, second: number, third: number) {
+    this.result = +first + +second + +third;
+  }
+
+  Multiply(first: number, second: number, third: number) {
+    this.result = first * second * third;
+  }
+
+  updateResult(first: number, second: number, third: number) {
+    if (this.selected == 1) {
+      this.Add(first, second, third);
+    } else {
+      this.Multiply(first, second, third);
+    }
+  }
 }
